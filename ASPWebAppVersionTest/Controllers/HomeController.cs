285f9eddb6cfb757310c0935ed4ef75666d42e89
Brian Arizmendi <brian.arizmendi@sidesoft.com.mx>
2024-03-13 13:20:07 -0600

    Add project files.

---
+++ ASPWebAppVersionTest/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+namespace ASPWebAppVersionTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ASPWebAppVersionTest/Properties/VersionInfo.cs
@@ -0,0 +1,2 @@
+using System.Reflection;
+[assembly: AssemblyVersion("1.0.11662.0")]
